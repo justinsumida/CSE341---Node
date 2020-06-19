02036dab847a1644517569bf7d45361b7d7456a1
justin-sumida <sum18004@byui.edu>
2020-06-19 12:23:47 -0700

    Prove 09

--- index.js
+++ index.js
@@ -6,5 +6,49 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/postageMath' , postageMath)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+function postageMath(req , res){
+  const shippingType = req.query.method;
+  const weight = Number(req.query.weight);
+  doMath(shippingType , weight, res);
+}
+
+function doMath(shippingType , weight, res){
+  let total = 0;
+  let price = 0;
+  let weightPrice = 0;
+  if(shippingType == "priority-express"){
+    price = 26.35;
+    if(weight > 1)  
+      weightPrice = weight * .4;
+    total = price + weightPrice;
+  }
+  else if(shippingType == "priority-mail"){
+    price = 7.50
+    if (weight > 1) 
+      weightPrice = weight * .6;
+    total = price + weightPrice;
+  }
+  else if(shippingType = "first-class-mail"){
+    price = 1;
+    if (weight > 1)
+      weightPrice = weight * .2;
+    total = price * weight; 
+  }
+  else if(shippingType = "first-class-package"){
+    price = 3.80;
+    total = price * weight;
+  }
+  else if(shippingType = "retail-ground"){
+    price = 7.50;
+    if (weight > 1)
+      weightPrice = weight  *.6;
+    total = price * weight;
+  }
+
+  const params = {shippingType: shippingType , weight: weight, total: total};
+
+  res.render('pages/postage' , params);
+}
